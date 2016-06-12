ecdaf3d4f0e70fb9257bc1d8d7b3bf5b83f2da52
SelinaWang <ziyu.wang@students.olin.edu>
2016-06-11 21:56:07 -0600

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -3,6 +3,8 @@ package com.flatironschool.javacs;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
@@ -13,6 +15,8 @@ import org.jsoup.select.Elements;
 public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
+	private static ArrayList<String> visitedURLs = new ArrayList<String>();
+    private static ArrayDeque<String> parenStack = new ArrayDeque<String>();
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -28,24 +32,112 @@ public class WikiPhilosophy {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+        String dest = "https://en.wikipedia.org/wiki/Philosophy";
+		String start = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		System.out.println("Starting on: "+start);
+		Integer limit = 20;
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		testPhilosophyConjecture(dest, start, limit);
+	}
+
+	public static void testPhilosophyConjecture(String dest, String start, Integer limit) throws IOException {
+		String url = start;
+		for (int i=0; i<limit; i++) {
+			if (visitedURLs.contains(url)) {
+				System.out.println("In a loop!");
+				return;
 			}
-        }
+			else {
+				visitedURLs.add(url);
+			}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+			Elements paragraphs = wf.fetchWikipedia(url);
+			Element firstLink = findFirstLink(paragraphs);
+
+			if (firstLink == null) {
+				System.out.println("Got to a page with no valid links");
+				return;
+			}
+
+			System.out.println(firstLink.text());
+			url = firstLink.absUrl("href");;
+
+			if (url.equals(dest)) {
+				System.out.println("We are on the 'Philosophy' Wikipedia page! Woohoo!");
+				return;
+			}
+		}
+	}
+
+	public static Element findFirstLink(Elements paragraphs) {
+		for (Element paragraph: paragraphs) {
+			Iterable<Node> iter = new WikiNodeIterable(paragraph);
+			for (Node node: iter) {
+				if (node instanceof TextNode) {
+					checkTextNode((TextNode)node);
+				}
+
+				if (node instanceof Element) {
+					Element validLink = isValidLink((Element) node);
+					if (validLink != null) {
+						return validLink;
+					}
+				}
+	        }
+		}
+		return null;
+	}
+
+	private static void checkTextNode(TextNode node) {
+		// populate parenStack
+		String text = node.text();
+		for (int i=0; i<text.length(); i++) {
+			if (text.charAt(i)=='(') {
+				parenStack.push("(");
+			}
+			if (text.charAt(i)==')') {
+				if (parenStack.isEmpty()) {
+					System.out.println("Unbalanced Parenthesis: Encountered ')' before '('");
+				}
+				parenStack.pop();
+			}
+		}
+
+	}
+
+	private static Element isValidLink(Element node) {
+		// Check if is a valid link
+		if (!node.tagName().equals("a")) {
+			return null;
+		}
+		if (!node.attr("href").startsWith("/wiki/")){
+			return null;
+		}
+		if (node.attr("href").endsWith("redlink=1")) {
+			return null;
+		}
+		if (isItalicized(node)) {
+			return null;
+		}
+		if (isInParen(node)) {
+			return null;
+		}
+		return node;
+	}
+
+	private static boolean isItalicized(Element first) {
+		// check if is italicized
+		for (Element node=first; node!=null; node=node.parent()) {
+			if (node.tagName().equals("i") || node.tagName().equals("em")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean isInParen(Element node) {
+		// check if is in parenthesis
+		return !parenStack.isEmpty();
 	}
 }
